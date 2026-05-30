71a73230d3fd18cb4c0af234c1803122eb5dacd9
a1ex <peterxhhx@gmail.com>
2026-05-30 17:07:11 +0800

    feat: init

--- apps/admin/.umirc.ts
+++ apps/admin/.umirc.ts
@@ -1,53 +1,13 @@
-import { defineConfig } from '@umijs/max';
+import { defineConfig } from 'umi';
 
 export default defineConfig({
-  antd: {},
-  access: {},
-  model: {},
-  initialState: {},
-  request: {},
-  layout: {
-    title: 'Link-Do Admin',
-    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
-    locale: false,
-  },
   routes: [
-    {
-      path: '/user',
-      layout: false,
-      routes: [
-        {
-          name: '登录',
-          path: '/user/login',
-          component: './Login',
-        },
-      ],
-    },
-    {
-      path: '/',
-      redirect: '/dashboard',
-    },
-    {
-      name: '首页',
-      path: '/dashboard',
-      component: './Dashboard',
-    },
-    {
-      name: 'To-Do 列表',
-      path: '/todos',
-      component: './Todos',
-    },
-    {
-      name: '用户管理',
-      path: '/users',
-      component: './Users',
-    },
+    { path: '/', component: 'index' },
+    { path: '/docs', component: 'docs' },
   ],
+
   npmClient: 'pnpm',
-  proxy: {
-    '/api': {
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    },
-  },
+  utoopack: {},
+  tailwindcss: {},
+  plugins: ['@umijs/plugins/dist/tailwindcss'],
 });
