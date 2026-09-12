316b98b542c1fb9de972f74a612b337e6f75bd98
a1ex <peterxhhx@gmail.com>
2026-09-12 11:50:26 +0800

    fix: bugfix

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,6 +3,24 @@ import Providers from "@/providers/base";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
+import { Merriweather, Montserrat, Ubuntu_Mono } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  variable: "--font-merriweather",
+});
+
+const ubuntuMono = Ubuntu_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ubuntu-mono",
+});
+
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -41,7 +59,7 @@ export default function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
       </head>
       <body
-        className={`${inter.className} text-pr bg-background flex h-screen flex-col overflow-hidden`}
+        className={`${montserrat.variable} ${merriweather.variable} ${ubuntuMono.variable} text-pr bg-background flex h-screen flex-col overflow-hidden`}
       >
         <Providers>
           <div className="bg-background flex-1 overflow-y-scroll">
